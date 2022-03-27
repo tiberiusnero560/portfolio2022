696eb26a97ba9fb988923252158be018bbb6d1fd
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2022-03-26 22:04:54 -0400

    Revert "Update Footer.js"
    This reverts commit b98d2ae65e44e6d7cb5a67e363f1d544d9df9b24.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,32 +7,8 @@ const Footer = () => {
         <div className='footer-container'>
             <ul>
                 <li>
-<<<<<<< Updated upstream
-                    <a href='
-                    '>
-                        <i className="fab fa-github"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href='
-                    '>
-                        <i className="fab fa-linkedin"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href='
-                    '>
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href='
-                    '>
-                        <i className="fab fa-facebook"></i>
-=======
-                    <a href="mailto:kyle.leblanc88@gmail.com?subject=" className="footer__link" className="contact-button-a">
+                    <a href="mailto:kyle.leblanc88@gmail.com?subject=" className="footer__link">
                         <button className="contact-button">Contact</button>
->>>>>>> Stashed changes
                     </a>
                 </li>
             </ul>
