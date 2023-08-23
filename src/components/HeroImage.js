6c480f3f783cc05448a0bf5256362c8d7c4883aa
tiberiusnero560 <kyle.leblanc88@gmail.com>
2023-08-23 09:10:53 -0400

    update hero image
    add a note that the website is being rebuilt

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -10,6 +10,8 @@ const HeroImage = () => {
         <h1 className='hero-container--title hero-container--h1'>Hi, I'm <strong className='kyle'>Kyle</strong></h1>
 
         <h2 className='hero-container--body'>I'm a <strong>Full Stack Web Developer</strong> and <strong>UI/UX Designer</strong> based in Ottawa, Canada.</h2>
+        <h2 className='hero-container--body'> This website is currently being rebuilt in NextJs with updated experience</h2>
+
       </div>
         <img className='heroimage-herocontainer--img' src={require('../assets/photos/HeroImage-1.jpg')} />
 
